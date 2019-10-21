1d8891bf9688c71067591eb06786490a203f10cc
Hmhicks4054 <54415500+Hmhicks4054@users.noreply.github.com>
2019-10-20 22:54:36 -0400

    Add files via upload

---
+++ integration project.py
@@ -0,0 +1,25 @@
+# My name is Herschel Hicks
+# This is a program to show my progress in coding in python
+
+import OutputDemo
+
+def main():
+    print("Welcome to my Integration Project!")
+continueProgram: True
+while continueProgram:
+        print("Enter the choice for what you would like to see")
+        print("1, Output formatting")
+        print("2. Area calculator")
+        print("Quit")
+        userChoice = int(input())
+        if userChoice == 1:
+
+elif userChoice == 2:
+    int(input("Enter the length of the side of a square to get the area:"))
+        print("calculateArea(side))
+        elif userChoice == 5:
+            continueProgram =  False
+    else: print(invalid selection. Try again")
+    def calculateArea (side):
+    return side * side
+main())
